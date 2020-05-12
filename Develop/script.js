76a9c94b99b303de421369ff8ae0bd74c3432e9e
Austin Gabe <57731755+thinkblue721@users.noreply.github.com>
2020-05-11 21:31:06 -0700

    Rearranged UI to add spaces for head banner, quote, tumblr/shop links, and gallery of images to be pulled from Instagram and displayed in modals

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,16 @@
-
 // on DOM load
-$( document ).ready(function() {
-    
-  });
+$(document).ready(function () {
+  $(document).foundation();
+  var queryURL = "https://api.instagram.com/oembed?url=https://www.instagram.com/p/B-dlmiynTdh/"
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+    var imgUrl = response.html;
+    var instagramHome = $("#instagramhome");
+    instagramHome.html(imgUrl);
+    window.instgrm.Embeds.process();
+  })
+});
 
-  
