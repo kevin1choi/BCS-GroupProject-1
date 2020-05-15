0faded478f042bb24283ef8541111e4aab410f6b
Austin Gabe <57731755+thinkblue721@users.noreply.github.com>
2020-05-15 15:01:56 -0700

    Added embedded Instagram posts and made them responsive

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,186 @@
 // on DOM load
 $(document).ready(function () {
-  $(document).foundation();
-  var queryURL = "https://api.instagram.com/oembed?url=https://www.instagram.com/p/B-dlmiynTdh/"
-  $.ajax({
-    url: queryURL,
-    method: "GET",
-  }).then(function (response) {
-    console.log(response);
-    var imgUrl = response.html;
-    var instagramHome = $("#instagramhome");
-    instagramHome.html(imgUrl);
-    window.instgrm.Embeds.process();
-  })
+
+  var queryURL = ["https://api.instagram.com/oembed?url=https://www.instagram.com/p/B_3ukUTnvST/", "https://api.instagram.com/oembed?url=https://www.instagram.com/p/B_1BdZgn0jv/", "https://api.instagram.com/oembed?url=https://www.instagram.com/p/B_gjv_gHt52/"];
+
+  var igOne = $("#instagramone")
+  var igTwo = $("#instagramtwo")
+  var igThree = $("#instagramthree")
+
+  var instagramDiv = [igOne, igTwo, igThree];
+
+  function setInstaFeed(count) {
+    if (count == 3) { return; }
+    $.ajax({
+      url: queryURL[count],
+      method: "GET"
+    }).then(function (response) {
+      // do the thing
+      console.log(response);
+      console.log(response.html)
+      var imgURL = response.html;
+      instagramDiv[count].html(imgURL);
+      window.instgrm.Embeds.process();
+    }).then(setInstaFeed(count + 1));
+  }
+  setInstaFeed(0);
+
+  // function instagramHome() {
+  //   $.ajax({
+  //     url: queryURL[i],
+  //     method: "GET",
+  //   }).then(function(response){
+  //     console.log(response);
+  //     console.log(response.html)
+  //     var imgURL = response.html;
+  //     instagramDiv[i].html(imgURL);
+  //     // for (i = 0; i < instagramDiv.length; i++) {
+  //     //   var instagramDiv[i].html(imgURL);
+  //     // };
+  //     window.instgrm.Embeds.process();
+  //   });
+  // }
+
+
+
+  // for (i = 0; i < queryURL.length; i++) {
+  //   $.ajax({
+  //     url: queryURL[i],
+  //     method: "GET",
+  //   }).then(function (response) {
+  //     console.log(response);
+  //     console.log(response.html)
+  //     var imgURL = response.html;
+  //     // instagramDiv[i].html(imgURL);
+  //     for (i = 0; i < instagramDiv.length; i++) {
+  //       var
+  //         instagramDiv[i].html(imgURL);
+  //     };
+  //     window.instgrm.Embeds.process();
+  //   });
+  // };
 });
 
+//   var queryURL = "https://api.instagram.com/oembed?url=https://www.instagram.com/p/B_3ukUTnvST/"
+//    $.ajax({
+//          url: queryURL,
+//          method: "GET",
+//      }).then(function(response){
+//          console.log(response);
+//          console.log(response.html)
+//          var imgUrl = response.html;
+
+//          var instagramOne = $("#instagramone");
+//          instagramOne.html(imgUrl);
+//          // replace with appropriate selectors from index.html
+//          // var testOne = $("");
+//          // testOne.html(imgUrl);             
+//      })
+//    var queryTwo = "https://api.instagram.com/oembed?url=https://www.instagram.com/p/B_1BdZgn0jv/"
+//    $.ajax({
+//        url: queryTwo,
+//        method: "GET",
+//    }).then(function(result){
+//        console.log(result);
+//        console.log(result.html);
+//        var imgUrlTwo = result.html;
+//        var testTwo = $("#instagramtwo");
+//        testTwo.html(imgUrlTwo);
+//    })
+//    var queryThree = "https://api.instagram.com/oembed?url=https://www.instagram.com/p/B_gjv_gHt52/"
+//    $.ajax({
+//        url: queryThree,
+//        method: "GET",
+//    }).then(function(outcome){
+//        console.log(outcome);
+//        console.log(outcome.html);
+//        var imgUrlThree = outcome.html;
+
+//        var testThree = $("#instagramthree");
+//        testThree.html(imgUrlThree);
+
+//        window.instgrm.Embeds.process()
+
+//    })
+
+// });
+
+
+// $(document).ready(function () {
+//   $(document).foundation();
+//   var queryURL = "https://api.instagram.com/oembed?url=https://www.instagram.com/p/B-dlmiynTdh/"
+//   $.ajax({
+//     url: queryURL,
+//     method: "GET",
+//   }).then(function (response) {
+//     console.log(response);
+//     var imgUrl = response.html;
+//     var instagramHome = $("#instagramhome");
+//     instagramHome.html(imgUrl);
+//     window.instgrm.Embeds.process();
+//   })
+// });
+
+
+
+
+
+
+
+
+// //load new random post every time page is refreshed
+// getRandom();
+// //variable retrieves a random index number
+// var x = Math.floor((Math.random() * 49) + 1);
+// console.log(x);
+// function getRandom() {
+//     var queryURL = 'https://api.tumblr.com/v2/blog/animatedtext.tumblr.com/posts?api_key=6zhnqA40ToF48oXKQFOVWRNfxfSTCFpO8xAJzWqUQOY3E1NOYj';
+//     $.ajax({
+//         url: queryURL,
+//         data: {
+//             //50 is the maximum number of posts that can be returned by the API
+//             limit: 50
+//         },
+//         dataType: 'jsonp',
+//         success: function (results) {
+//             console.log(results);
+//             var p = results.response.posts;
+//                 var type = p[x].type;
+//                 var caption = p[x].caption;
+//                 var notes = p[x].note_count + " notes";
+//                 var source = "Source: " + p[i].source_title;
+//                 var id = p[x].id;
+//                 var tagsArray = p[x].tags;
+//                 //create an array of tags for each post
+//                 for (var k = 0; k < tagsArray.length; k++) {
+//                     tagsArray[k] = " #" + tagsArray[k];
+//                     tags = " " + tagsArray;
+//                 }
+//                 //retrieve the image associate with the post
+//                 if (type == "photo") {
+//                     var photos = p[x].photos;
+//                     for (var j = 0; j < photos.length; j++) {
+//                         if (photos[j].alt_sizes[0]) {
+//                             imgURL = photos[j].alt_sizes[0].url
+//                         }
+//                     }
+//                 }
+//                 i++;
+//                 //filter content types that cause double-posting
+//                 if (type == "answer" || type == "text") {
+//                     console.log(p[x].summary + ", " + id);
+//            } else {
+//                //render posts to the page
+//                $("#tumblr-posts").append(imgContainer + imgURL + cardSection + caption + sectionParagraph + source + tags + cardNotes + notes + cardBottom);
+//             }
+//         }
+//     });
+// }
+// //These variables render the post on the fly
+// var imgContainer =   "<div class='grid-x grid-margin-x' style='padding:10px;'><div class='cell medium-2'></div><div class='cell medium-8 card'> <img src='";
+// //+ imgURL + //
+// var cardSection = "/><div class='card-section'><p>";
+// // + source + tags +//
+// var cardNotes = "/p><div class='grid-x grid-margin-x'><div class='cell auto'><div class='notes'>"
+// // + notes + //
+// var cardBottom = "</div></div><div class='cell auto'><div class='float-right'><button data-open='askModal'><small>request a gif! </small></button><i class='fi-mail' id='contact-me' data-open='contactModal'></i><i class='fi-share' id='share' data-open='shareModal'></i></div></div></div></div></div><div class='cell medium-2'></div></div>"
